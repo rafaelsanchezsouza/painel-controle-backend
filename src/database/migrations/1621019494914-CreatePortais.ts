5dc6b4bc3df33e39c83a9b1db7e09cb6668d9ab1
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-05-14 16:35:34 -0300

    feat: cria tabela portais

---
+++ src/database/migrations/1621019494914-CreatePortais.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePortais1621019494914 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'portais',
+        columns: [
+          {
+            name: 'cpnj',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'nomeBase',
+            type: 'varchar',
+          },
+          {
+            name: 'nomenclatura',
+            type: 'varchar',
+          },
+          {
+            name: 'vencimento',
+            type: 'varchar',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('portais');
+  }
+}
